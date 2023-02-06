b4cb632899ca7f81b1550c19734565cb67c90bc7
Tim-ksvr <102039667+Tim-ksvr@users.noreply.github.com>
2023-02-06 17:37:59 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-/app/ctg -L="ss+mws://$METHOD:$PASSWORD@localhost:3580" -L="socks5+mws://$USER:$PASSWORD@localhost:3553?path=$WSPATH" >/dev/null 2>&1 &
+/app/ctg -L="ss+mws://$METHOD:$PASSWORD@localhost:3580" -L="socks5+mws://$USER:$PASSWORD@localhost:3553?path=/$WSPATH" >/dev/null 2>&1 &
 ssh-keygen -A
 /usr/sbin/sshd -D -e "$@" &
 crond
