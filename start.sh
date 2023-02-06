42d68c39116b3b81538f6f3121c26b292fc8201c
Tim-ksvr <102039667+Tim-ksvr@users.noreply.github.com>
2023-02-06 17:45:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 /app/ctg -L="ss+mws://$METHOD:$PASSWORD@localhost:3580" -L="socks5+mws://$USER:$PASSWORD@localhost:3553?path=/$WSPATH" >/dev/null 2>&1 &
 ssh-keygen -A
+sed -i "s/wspath/$WSPATH/" /etc/nginx/http.d/default.conf
 /usr/sbin/sshd -D -e "$@" &
 crond
 nginx -g 'daemon off;'
